269902fc38f5083bbb7174bf13ec0d8ff0374d85
jeje <bang710109@gmail.com>
2026-02-10 19:48:27 +0900

    update name

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,7 +4,9 @@ export default function Name() {
   return (
     <section id="namebox">
       <div className="name-inner">
-        <h2 className="name-title">Frontend Developer 방서진</h2>
+        <h2 className="name-title">
+          UI/UX Web Publisher & Frontend Developer 방서진
+        </h2>
       </div>
     </section>
   );
