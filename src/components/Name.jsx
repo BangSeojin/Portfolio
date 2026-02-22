aaed94840dbc6f6d5cb1f8fb9b42d15de2a61557
jeje <bang710109@gmail.com>
2026-02-22 12:56:32 +0900

    썸네일 수정 및 Name.jsx 수정

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,9 +4,7 @@ export default function Name() {
   return (
     <section id="namebox">
       <div className="name-inner">
-        <h2 className="name-title">
-          UI/UX Web Publisher & Frontend Developer 방서진
-        </h2>
+        <h2 className="name-title">UI/UX Web Publisher 방서진</h2>
       </div>
     </section>
   );
